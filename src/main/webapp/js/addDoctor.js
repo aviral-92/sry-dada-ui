9fb65cdd19564d5808d8660f6fc9181429d9b2d8
Aviral Mittal <amittal@xavient.com>
2017-01-06 16:59:48 +0530

    Doctor functionality updated.

--- src/main/webapp/js/addDoctor.js
+++ src/main/webapp/js/addDoctor.js
@@ -1,47 +1,56 @@
-	function submitform() {
-	var doctorGovtServent = '';
-		if($('#govtservent').is(':checked')){
-			doctorGovtServent = 'true';
-		} else {
-			doctorGovtServent = 'false';
-		}
-		var formData = "{ \"doctorId\":" + $('#id').val()
-				+ ", \"doctorName\" : \"" + $('#name').val()
-				+ "\", \"doctorNumber\" : \"" + $('#number').val()
-				+ "\", \"doctorHomeAddress\" : \"" + $('#address').val()
-				+ "\", \"doctorAdhaarNumber\" : \"" + $('#adhaarnumber').val()
-				+ "\", \"doctorHighestDegree\" : \""
-				+ $('#highestdegree').val() + "\", \"doctorExpertized\" : \""
-				+ $('#expertise').val() + "\", \"doctorGovtServent\" : "
-				+ doctorGovtServent
-				+ ", \"doctorOneTimeConsultingFee\" : \""
-				+ $('#consultingfee').val()
-				+ "\", \"doctorDaystoCheckFreeInConsultingFee\" : \""
-				+ $('#daysfreeconsultingfees').val()
-				+ "\", \"doctorShopAddress\" : \"" + $('#clinicaddress').val()
-				+ "\"}";
-		console.log(formData);
-		$.ajax({
-			url : "http://localhost:9090/adddoctor",
-			headers: {
-				'Content-Type': 'application/json',
-				'Access-Control-Request-Method' : 'POST'
-            },
-			data : formData,
-			type : "POST",
-			contentType : "application/json; charset=utf-8",
-			dataType : "json",
-			success : function(data) {
-				alert("Successfully Inserted...!!!	"+JSON.stringify(data));
-				// console.log(data);
-				var obj = JSON.parse(JSON.stringify(data));
-				// alert(obj.msg);
-				$('#message').html(obj.msg);
-				$('#message').show();
-
-			},
-			error : function(data) {
-				alert("Error");
-			}
-		});
+/*function submitform() {
+var doctorGovtServent = '';
+	if($('#govtservent').is(':checked')){
+		doctorGovtServent = 'true';
+	} else {
+		doctorGovtServent = 'false';
 	}
+	var formData = "{ \"doctorId\":" + $('#id').val()
+			+ ", \"doctorName\" : \"" + $('#name').val()
+			+ "\", \"doctorNumber\" : \"" + $('#number').val()
+			+ "\", \"doctorHomeAddress\" : \"" + $('#address').val()
+			+ "\", \"doctorAdhaarNumber\" : \"" + $('#adhaarnumber').val()
+			+ "\", \"doctorHighestDegree\" : \""
+			+ $('#highestdegree').val() + "\", \"doctorExpertized\" : \""
+			+ $('#expertise').val() + "\", \"doctorGovtServent\" : "
+			+ doctorGovtServent
+			+ ", \"doctorOneTimeConsultingFee\" : \""
+			+ $('#consultingfee').val()
+			+ "\", \"doctorDaystoCheckFreeInConsultingFee\" : \""
+			+ $('#daysfreeconsultingfees').val()
+			+ "\", \"doctorShopAddress\" : \"" + $('#clinicaddress').val()
+			+ "\"}";
+	console.log(formData);
+	$.ajax({
+		url : "http://localhost:9090/adddoctor",
+		headers: {
+			'Content-Type': 'application/json',
+			'Access-Control-Request-Method' : 'POST'
+        },
+		data : formData,
+		type : "POST",
+		contentType : "application/json; charset=utf-8",
+		dataType : "json",
+		success : function(data) {
+			alert("Successfully Inserted...!!!	"+JSON.stringify(data));
+			// console.log(data);
+			var obj = JSON.parse(JSON.stringify(data));
+			// alert(obj.msg);
+			$('#message').html(obj.msg);
+			$('#message').show();
+
+		},
+		error : function(data) {
+			alert("Error");
+		}
+	});
+}*/
+
+var app = angular.module('myApp', []);
+app.controller('addDoctorController', function($scope) {
+
+	$scope.isVisible = false;
+	$scope.ShowHide = function() {
+	$scope.isVisible = $scope.isVisible ? false : true;
+	}
+});
--- src/main/webapp/jsp/AddDoctor.jsp
+++ src/main/webapp/jsp/AddDoctor.jsp
@@ -1,7 +1,7 @@
 <html>
 <head>
 <title>Insert title here</title>
-<link rel="stylesheet" type="text/css" href="css/home.css">
+<link rel="stylesheet" type="text/css" href="css/AddDoctor.css">
 <script src="js/jquery-latest.min.js" type="text/javascript"></script>
 <script
 	src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.min.js"></script>
@@ -11,10 +11,48 @@
 <body ng-app="myApp">
 
 	<div ng-controller="addDoctorController">
-		<div>
-			to add doctor <a href="#">Click here</a>
+		<div class="clickText">
+			To add doctor click <a ng-click="ShowHide()">Here</a> 
 		</div>
-		<div class="addForm" ng-show="isVisible"></div>
+		<div class="addForm" ng-show="isVisible">
+			<table>
+				<tr>
+					<td><label>Doctor Name</label></td>
+					<td><input type="text" ng-model="doctorName" /></td>
+				</tr>
+				<tr>
+					<td><label>Doctor Mobile Number</label></td>
+					<td><input type="text" ng-model="doctorMobileNumber" /></td>
+				</tr>
+				<tr>
+					<td><label>Doctor Adhar Number</label></td>
+					<td><input type="text" ng-model="doctorAdharNumber" /></td>
+				</tr>
+				<tr>
+					<td><label>Doctor Home Address</label></td>
+					<td><input type="text" ng-model="doctorHomeAddress" /></td>
+				</tr>
+				<tr>
+					<td><label>Doctor Highest Degree</label></td>
+					<td><input type="text" ng-model="doctorHighestDegree" /></td>
+				</tr>
+				<tr>
+					<td><label>Doctor Expertise</label></td>
+					<td><input type="text" ng-model="doctorExpertise" /></td>
+				</tr>
+				<tr>
+					<td><label>Doctor Government</label></td>
+					<td>Yes<input type="radio" name="mygroup"
+						ng-model="doctorName" /> No<input type="radio" name="mygroup"
+						ng-model="doctorName" /></td>
+				</tr>
+				<tr>
+					<td><label>Click to Add</label></td>
+					<td><input type="button" ng-click="doctorAdd()"
+						value="Add Doctor" /></td>
+				</tr>
+			</table>
+		</div>
 	</div>
 </body>
 </html>
