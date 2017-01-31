f88a7e4e70d5f781dd8fea6441d9fbe1ab799373
Mahima-95 <agg.mahima@gmail.com>
2017-01-31 23:06:13 +0530

    add-> updatePatient in patient login...!!!!!

--- src/main/webapp/js/DoctorRouter.js
+++ src/main/webapp/js/DoctorRouter.js
@@ -1,4 +1,4 @@
-var scotchApp = angular.module('myApp', [ 'ngRoute' , 'UserValidation']);
+var scotchApp = angular.module('myApp', [ 'ngRoute', 'UserValidation' ]);
 
 scotchApp.config(function($routeProvider) {
 	$routeProvider
@@ -8,47 +8,51 @@ scotchApp.config(function($routeProvider) {
 		templateUrl : '/html/Container.html',
 		controller : 'middleContent'
 	})
-	
+
 	// route for the login page
 	.when('/login', {
 		templateUrl : '/html/Login.html',
 		controller : 'login'
 	})
-	
+
 	// route for the login Success page
 	.when('/drLoginSuccess', {
 		templateUrl : '/html/Dr_LoginSuccess.html',
 		controller : 'drLoginSuccess'
 	})
-	
+
 	// route for the patient login page
 	.when('/patientlogin', {
 		templateUrl : '/html/PatientLogin.html',
 		controller : 'patientlogin'
 	})
 
+	// route for the patient login Success page
+	.when('/patientLoginSuccess', {
+		templateUrl : '/html/patientLoginSuccess.html',
+		controller : 'patientLoginSuccess'
+	})
 	// route for the SignUp page
 	.when('/signUp', {
 		templateUrl : '/html/DoctorSignUp.html',
 		controller : 'signUp'
 	})
-	
+
 	// route for the patient signup page
 	.when('/patientsignup', {
 		templateUrl : '/html/PatientSignUp.html',
 		controller : 'patientsignup'
 	})
-	
+
 	// route for the About page
 	.when('/about', {
 		templateUrl : '/html/about.html',
 		controller : 'about'
 	})
 
-	// route for the Contact page	
+	// route for the Contact page
 	.when('/contact', {
 		templateUrl : '/html/contact.html',
 		controller : 'contact'
 	});
 });
-
--- src/main/webapp/js/Patientcontrol.js
+++ src/main/webapp/js/Patientcontrol.js
@@ -1,7 +1,93 @@
 scotchApp.controller('patientlogin',function($scope){
+	alert("patient login");
 	
+	$scope.patientLogin = function(loginDetails){
+		console.log(loginDetails);
+		if(loginDetails.password == 'user'){
+			$scope.message = 'Successfully logged in....!!!!!!';
+			$scope.login = loginDetails;
+			window.location="#/patientLoginSuccess";
+		}else{
+			$scope.message = 'Invalid Credentials...!!!';
+		}
+	}
 });
 
+// Patient Login Success Starts---------------------------------------------------------------------------------------
+scotchApp.controller('patientLoginSuccess', function($scope, $rootScope, $http){
+	
+	$scope.patientUpdate = false;
+	$scope.newValue = function(value){
+		console.log(value);
+		if(value=='update'){
+			$scope.patientUpdate = true;
+			$scope.demo = function(patient){
+				console.log(">>>>>>>>>>>> Demo" +patient.patientId);
+				var patientGet = null;
+				if(patient!=null && patient.patientId!=null && patient.patientId!=""){
+					patientGet = $http.get('https://doctor-service.cfapps.io/doctor-management/getdoctorbyid/'+patient.patientId);
+					patientUpdateAjax(patientGet, $scope, $http);
+					$scope.modalBody = true;
+					}
+				else if(patient!=null && patient.patientMobileNumber != null && patient.patientMobileNumber!=""){
+					// Ajax on basis of patientMobileNumber.
+					patientGet = $http.get('https://doctor-service.cfapps.io/doctor-management/getdoctorbymobilenumber/'+patient.patientMobileNumber);
+					// Function called
+					patientUpdateAjax(patientGet, $scope, $http);
+					$scope.modalBody = true;
+					
+				}else if(patient != null && patient.patientAdharNumber != null && patient.patientAdharNumber != ""){
+					// Ajax on basis of patientAdhaarNumber.
+					patientGet = $http.get('https://doctor-service.cfapps.io/doctor-management/getdoctorbyadharNumber/'+patient.patientAdharNumber);
+					// Function called
+					patientUpdateAjax(patientGet, $scope, $http);
+					$scope.modalBody = true;
+				}else{
+					$scope.modalBody = false;
+					$scope.modalBodyMsg = " Please provide input";
+				}
+				}
+			}else{
+				$scope.patientUpdate = false;
+			}
+		}
+});
+
+function patientUpdateAjax(patientGet, $scope, $http){
+	
+	// Get before update...
+	patientGet.success(function(data) {
+		$scope.patients = data;
+		console.log($scope.patients);
+		
+		// if no value found then it will display this
+		if(data.patientId == null){
+			$scope.modalBody = false;
+			$scope.modalBodyMsg = " Please provide correct value.";
+		}
+		// Update function calls
+		$scope.patientUpdate = function(patientUpdateValue){
+			console.log(patientUpdateValue);
+			// Update Ajax hit
+			var updatepatient = $http.put('https://doctor-service.cfapps.io/doctor-management/updatedoctor', patientUpdateValue);
+			// For success
+			updatepatient.success(function(updateResponse) {
+				$scope.patientUpdate = updateResponse.message;
+			});
+			// For error
+			patientGet.error(function(updateResponse, status, headers, config) {
+				alert("failure message: " + updateResponse.message);
+			});
+		}
+	});
+	patientGet.error(function(data, status, headers, config) {
+		alert("failure message: " + data.message);
+		$scope.modalBody = false;
+		$scope.modalBodyMsg = " Please provide correct value.";
+	});
+	return;
+}
+//Patient Login Success Ends---------------------------------------------------------------------------------------
 //patient sign up Starts------------------------------------------------------------------------------------------------
 scotchApp.controller('patientsignup',function($scope, $http){
 	$scope.patientAdd = function(patient, formName) {
@@ -23,7 +109,7 @@ scotchApp.controller('patientsignup',function($scope, $http){
 }
 	$scope.doBlurName = function($event){
 		var target = $event.target;
-		if($scope.doctor != null && $scope.patient.patientName.length > 0){
+		if($scope.patient != null && $scope.patient.patientName.length > 0){
 			target.blur();	
 		}else{
 			target.focus();
