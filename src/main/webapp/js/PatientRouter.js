1f73cac0d3c899487b76f32b14ae30847e842583
Mahima-95 <agg.mahima@gmail.com>
2017-02-08 18:23:10 +0530

    Patient profile and dashboard added...

---
+++ src/main/webapp/js/PatientRouter.js
@@ -0,0 +1,61 @@
+scotchApp.config(function($routeProvider) {
+	$routeProvider
+
+	// route for the patient login page
+	.when('/patientlogin', {
+		templateUrl : '/html/PatientService/PatientLogin.html',
+		controller : 'patientlogin'
+	})
+
+	// route for the patient login Success page
+	.when('/patientLoginSuccess', {
+		templateUrl : '/html/PatientService/patientLoginSuccess.html',
+		controller : 'patientLoginSuccess'
+	})
+	
+	// route for the patient signup page
+	.when('/patientsignup', {
+		templateUrl : '/html/PatientService/PatientSignUp.html',
+		controller : 'patientsignup'
+	})
+
+	// route for the About page
+	.when('/about', {
+		templateUrl : '/html/about.html',
+		controller : 'about'
+	})
+
+	// route for the Contact page
+	.when('/contact', {
+		templateUrl : '/html/contact.html',
+		controller : 'contact'
+	})
+
+	// route for dashboard update profile
+	.when('/patientupdateProfile', {
+		templateUrl : '/html/PatientDashboard/PatientupdateProfile.html',
+		controller : 'patientupdateProfile'
+	})
+
+	// route for dashboard retrieve password
+	.when('/retrievePassword', {
+		templateUrl : '/html/Dashboard/RetrievePassword.html',
+		controller : 'retrievePassword'
+	})
+
+	// route for the Dashboard page
+	.when('/patientdashboard', {
+		templateUrl : '/html/PatientDashboard/WelcomePatientDashboard.html',
+		controller : 'patientdashboard'
+	})
+	// route for patient dashboard home page
+	.when('/patientafterLogin', {
+		templateUrl : '/html/PatientDashboard/PatientAfterLogin.html',
+		controller : 'patientafterLogin'
+	})
+	// route for the logout page
+	.when('/patientlogout', {
+		templateUrl : '/html/PatientService/PatientLogin.html',
+		controller : 'patientlogout'
+	});
+});
--- src/main/webapp/js/Patientcontrol.js
+++ src/main/webapp/js/Patientcontrol.js
@@ -1,18 +1,69 @@
-scotchApp.controller('patientlogin',function($scope){
-	alert("patient login");
+scotchApp.controller('patientlogin',function($scope, $rootScope, $http, $cookieStore, $window){
 	
-	$scope.patientLogin = function(loginDetails){
-		console.log(loginDetails);
-		if(loginDetails.password == 'user'){
-			$scope.message = 'Successfully logged in....!!!!!!';
-			$scope.login = loginDetails;
-			window.location="#/patientLoginSuccess";
-		}else{
-			$scope.message = 'Invalid Credentials...!!!';
+	if($cookieStore.get('loginData') == undefined || $cookieStore.get('email') == undefined){
+		$scope.patientLogin = function(loginDetails){
+			console.log(loginDetails);
+			$cookieStore.put('email', loginDetails.email);
+			// TODO need to change with email.
+			var loginSuccessful = $http.get('http://patient-service.cfapps.io/patient/getPatientByEmail/'+loginDetails.email);
+			console.log(">>>>>>>>>" + loginSuccessful.success);
+			
+			loginSuccessful.success(function(getPatientDetails) {
+				if(getPatientDetails.patientId != null){
+					$scope.message = 'Successfully Logged in...!!!';
+					//$rootScope.getPatientByMobile = getPatientDetails; // TODO need to change by Email...
+					$cookieStore.put('loginData', getPatientDetails);
+					window.location = "#/patientdashboard";
+				}else{
+					$scope.message = 'Invalid Credentials...!!!';
+		    		}
+			});
+			loginSuccessful.error(function(data, status, headers, config) {
+				alert("failure message: " + data.message);
+				$scope.message = 'Invalid Credentials...!!!';
+			});
+			
 		}
+	}else{
+		$window.location.href = "#/patientdashboard";
 	}
 });
 
+scotchApp.controller('patientlogout',function($scope, $rootScope, $http, $cookieStore, $window){
+	
+	$cookieStore.remove('email');
+	$cookieStore.remove('loginData');
+	window.location = "#/patientlogin";
+});
+scotchApp.controller('patientdashboard',function($scope, $rootScope, $http, $cookieStore){
+	var patientDetail = $cookieStore.get('loginData');
+	var fieldCounter = checkDoctorField(patientDetail);
+	$scope.percent = parseInt((fieldCounter /7)*100)+'%';
+	
+});
+
+function checkDoctorField(patients){
+	
+	if(patients != null){
+		var field = 1;
+		if(patients.homeAddress != null){
+			field++;
+		}
+		return field;
+	}
+}
+function getByEmail($http, $cookieStore){
+	
+	alert($cookieStore.get('email'));
+	var patients = $http.get('http://patient-service.cfapps.io/patient/getPatientByEmail/'+$cookieStore.get('email'));
+	patients.success(function(data) {
+		return data;
+	});
+	// For error
+	doctors.error(function(data, status, headers, config) {
+		// alert("failure message: " + updateResponse.message);
+	});
+}
 // Patient Login Success Starts---------------------------------------------------------------------------------------
 scotchApp.controller('patientLoginSuccess', function($scope, $rootScope, $http){
 	//http://patient-service.cfapps.io/patient/getpatientByName
@@ -182,13 +233,101 @@ scotchApp.controller('patientsignup',function($scope, $http){
 			target.focus();
 		}
 	}
-	$scope.doBlurHomeAddress = function($event){
+/*	$scope.doBlurHomeAddress = function($event){
 		var target = $event.target;
 		if($scope.patient != null && $scope.patient.patientHomeAddress != null && $scope.patient.patientHomeAddress.length > 0){
 			target.blur();	
 		}else{
 			target.focus();
 		}
-}
+}*/
 });
 //patient sign up Ends------------------------------------------------------------------------------------------------
+scotchApp.controller('patientupdateProfile',function($scope, $rootScope, $http, $cookieStore){
+	
+	$scope.patients = $cookieStore.get('loginData');
+	
+	$scope.patientUpdate = function(patientUpdateValue){
+		console.log(patientUpdateValue);
+		// Update Ajax hit
+		var updatepatient = $http.put('http://patient-service.cfapps.io/patient/', patientUpdateValue);
+		// For success
+		updatepatient.success(function(updateResponse) {
+			$scope.successMessage = "Successfully Updated...!!!";
+
+			// Rest hit to get data and refresh cookies......Strats
+			var patientSuccess = $http.get('http://patient-service.cfapps.io/patient/getPatientByEmail/'+patients.patientEmail);
+			patientSuccess.success(function(data) {
+				alert("dfdfdf");
+				console.log(data.mobile);
+				$cookieStore.remove('loginData');
+				$cookieStore.put('loginData', data);
+			});
+			// For error
+			patientSuccess.error(function(data, status, headers, config) {
+				// alert("failure message: " + updateResponse.message);
+			});
+			// Rest hit to get data and refresh cookies......Ends
+		});
+		// For error
+		updatepatient.error(function(updateResponse, status, headers, config) {
+			alert("failure message: " + updateResponse.message);
+		});
+	}
+	
+	// validation Added......................
+	$scope.doBlurName = function($event){
+		var target = $event.target;
+		if($scope.patients != null && $scope.patients.patientName.length > 0){
+			target.blur();	
+		}else{
+			target.focus();
+		}
+	}
+	$scope.doBlurMobile = function($event){
+		var target = $event.target;
+		if($scope.patients != null && $scope.patients.patientMobile != null && $scope.patients.patientMobile.length == 10){
+			target.blur();	
+		}else{
+			target.focus();
+		}
+	}
+
+	$scope.doBlurHomeAddress = function($event){
+		var target = $event.target;
+		if($scope.patients != null && $scope.patients.patientHomeAddress != null && $scope.patients.patientHomeAddress.length > 0){
+			target.blur();	
+		}else{
+			target.focus();
+		}
+	}
+
+	
+	// check validation
+});
+scotchApp.controller('retrievePassword',function($scope, $rootScope){
+	$scope.submit = function(){
+        /* $scope.message = "Password send to your E-mail Id"; */
+        alert("Password send to your E-mail Id");
+	}
+});
+
+scotchApp.controller('patientafterLogin',function($scope, $rootScope, $cookieStore){
+	
+	
+	if($cookieStore.get('loginData') != undefined && $cookieStore.get('email') != undefined){
+
+		console.log("<<<<<<<<<<<<" +$cookieStore.get('loginData'));
+		var getLoginDetails = $cookieStore.get('loginData');
+		if(getLoginDetails.gender == '0'){
+			getLoginDetails.gender = 'Female';
+		}else{
+			getLoginDetails.gender = 'Male';
+		}
+		
+		$scope.patient = getLoginDetails;
+		
+	}else{
+		window.location = "#/patientlogin";
+	}
+});
