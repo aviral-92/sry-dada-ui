81aec1903ec63c00e3533ce9d2da65033e6effbd
Aviral Mittal <aviralmittal2012@gmail.com>
2017-03-02 11:52:16 +0530

    Angular added

--- src/main/webapp/Dashboard/Controller/DashboardController.js
+++ src/main/webapp/Dashboard/Controller/DashboardController.js
@@ -4,17 +4,37 @@ scotchApp.controller('index', function($scope) {
 
 scotchApp.controller('home', function($scope, $http) {
     
-    $scope.click == false;
+    $scope.visible = false;
+    var index = 0;
     $scope.url = "#/home";
     var todo = $http.get('/js/MockJson/todoList.json');
     todo.success(function(todoData) {
         $scope.todoList = todoData;
     });
     
-    $scope.click = function(){
-        alert('gg');
-        $scope.click == true;
+    /*$scope.edit = function(todoData){
+        
+        $scope.visible = true;
+        index = $scope.todoList.indexOf(todoData);
     }
+    
+    $scope.edited = function(){
+        $scope.todoList.push({
+            //$scope.
+            //"message":$scope.todoMessage
+        });
+        $scope.todoList.splice(index, 0, $scope.todoMessage);
+        $scope.visible = false;
+    }*/
+    
+    $scope.close = function(){
+        var data = $scope.todoTastData;
+        $scope.todoList.push({
+            "message":data
+        });
+        $scope.todoTastData = '';
+    }
+    
 });
 
 scotchApp.controller('calender', function($scope) {
