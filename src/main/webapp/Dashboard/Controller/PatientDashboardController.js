d394b5fcfca5c32a5cdb31935306ef44c799e4d2
Mahima-95 <agg.mahima@gmail.com>
2017-03-03 15:27:12 +0530

    doctorLogin updated, Patient dashboard, date with angular material, json & profile updated...

--- src/main/webapp/Dashboard/Controller/PatientDashboardController.js
+++ src/main/webapp/Dashboard/Controller/PatientDashboardController.js
@@ -15,11 +15,28 @@ scotchApp.controller('patientHome', function($scope, $http) {
     }
 });
 
-scotchApp.controller('patientProfile', function($scope,$cookieStore) {
+scotchApp.controller('patientProfile', function($scope,$cookieStore, fileReader) {
    
 	 $scope.url = "#/patientProfile";
 	    var getPatients = $cookieStore.get('loginData');
 	    
+	    $scope.patients = getPatients
+	    $scope.patients.dob = new Date($scope.patients.dob);
+	    
+	    $scope.uploadPicture = function () {
+	        var fileInput = document.getElementById('uploadFile');
+	        fileInput.click();
+	      };
+	      
+	      
+	      $scope.getFile = function () {
+	        fileReader.readAsDataUrl($scope.file, $scope)
+	            .then(function (result) {
+	              $scope.patients.src = result;
+	            console.log($scope.file);
+	            });
+	      };
+	      
 	    $scope.patients = getPatients
 	    $scope.patientUpdate = function(patientUpdateValue) {
 	        console.log(patientUpdateValue);
@@ -54,6 +71,34 @@ scotchApp.controller('patientProfile', function($scope,$cookieStore) {
 	    //Calucate Age of Patient
 	    var age = new Date().getYear() - new Date($scope.patients.dob).getYear();
 	    $scope.patients.age = age;
-	    
+	  //Calculate percentage dynamically...
+	    if (getPatients != null) {
+	            var field = 5;
+	            if (getPatients.patientHomeAddress != null &&
+	            		getPatients.patientHomeAddress != 'NA') {
+	                field++;
+	            }
+	            if (getPatients.dob != null &&
+	            		getPatients.dob != 'NA') {
+	                field++;
+	            }
+	            if (getPatients.gender != null &&
+	            		getPatients.gender != 'NA') {
+	                field++;
+	            }
+	            if (getPatients.age != null &&
+	            		getPatients.age != 'NA') {
+	                field++;
+	            }
+	            if (getPatients.description != null &&
+	            		getPatients.description != 'NA') {
+	                field++;
+	            }
+	            if (getPatients.allergies != null &&
+	            		getPatients.allergies != 'NA') {
+	                field++;
+	            }
+	            $scope.percent = parseInt((field / 11) * 100) + '%';
+	        }
 });
 
