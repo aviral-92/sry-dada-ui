0b150b2e20f116799033c71d66f987b90b6af065
Mahima-95 <agg.mahima@gmail.com>
2017-03-06 14:22:26 +0530

    Admin pages of about, contact, doctor, patient pages created & update indexpage with ajax hit & AIM is updated with image &textOnImage, notifications remove from doctor& patient Dashboard page...

--- src/main/webapp/Admin/Controller/AdminController.js
+++ src/main/webapp/Admin/Controller/AdminController.js
@@ -1,18 +1,65 @@
 scotchApp.controller('adminIndex', function($scope) {
-    
-});
-
-scotchApp.controller('adminHome', function($scope,$cookieStore) {
 
 });
 
-scotchApp.controller('adminIndexPage', function($scope) {
-    
+scotchApp.controller('adminHome', function($scope, $cookieStore) {
+
 });
 
-/*scotchApp.controller('signout', function($scope,$cookieStore, $window) {
-	   
-    $cookieStore.remove('email') ;
-    $cookieStore.remove('loginData') ;
-    $window.location.href = '/index.html#/';
-});*/
+scotchApp.controller('adminIndexPage', function($scope, $http) {
+	
+	var response = $http.get('https://dynamicuiservice.cfapps.io/firstpage/getall');
+	response.success(function(getDataOnAjax) {
+		$scope.getDatas = getDataOnAjax;
+	});
+	response.error(function(data, status, headers, config) {
+		alert("Error");
+	});
+	
+	/*$scope.adminIndex = function(updateData) {
+		var details = null;
+		if (updateData != null) {
+			$scope.message = null;
+			console.log(updateData);
+//			details = 
+		} else {
+			$scope.message = "please provide input..!!!";
+		}
+
+		if (details != null) {
+			details.success(function(getData) {
+				console.log(">>>>>>>" + getData[0].mobile);
+				$scope.doctors = getData;
+				$scope.modalBody = true;
+				$scope.loader = false;
+			});
+			details.error(function(data, status, headers, config) {
+				alert("failure message: " + data.message);
+				$scope.message = 'No Data Found!!!';
+			});
+		}
+	}*/
+});
+
+scotchApp.controller('adminAboutUsPage', function($scope) {
+
+});
+
+scotchApp.controller('adminContactUsPage', function($scope) {
+
+});
+
+scotchApp.controller('adminDoctorPage', function($scope) {
+
+});
+
+scotchApp.controller('adminPatientPage', function($scope) {
+
+});
+scotchApp.controller('signout', function($scope, $cookieStore, $window) {
+
+	/*
+	 * $cookieStore.remove('email') ; $cookieStore.remove('loginData') ;
+	 */
+	$window.location.href = '/index.html#/';
+});
--- src/main/webapp/Admin/Controller/AdminRouter.js
+++ src/main/webapp/Admin/Controller/AdminRouter.js
@@ -8,9 +8,29 @@ scotchApp.config(function($routeProvider) {
 		templateUrl : 'Admin/pages/AdminHome.html',
 		controller : 'adminHome'
 	})
-    
+    // route for the index page
     .when('/adminIndexPage', {
 		templateUrl : 'Admin/pages/AdminIndexPage.html',
 		controller : 'adminIndexPage'
+	})
+	// route for the about us page
+	 .when('/adminAboutUsPage', {
+		templateUrl : 'Admin/pages/AdminAboutUsPage.html',
+		controller : 'adminAboutUsPage'
+	})
+	// route for the contact us page
+	 .when('/adminContactUsPage', {
+		templateUrl : 'Admin/pages/AdminContactUs.html',
+		controller : 'adminContactUsPage'
+	})
+	// route for the doctor page
+	.when('/adminDoctorPage', {
+		templateUrl : 'Admin/pages/AdminDoctorPage.html',
+		controller : 'adminDoctorPage'
+	})
+	// route for the patient page
+	.when('/adminPatientPage', {
+		templateUrl : 'Admin/pages/AdminPatientPage.html',
+		controller : 'adminPatientPage'
 	});
 });
